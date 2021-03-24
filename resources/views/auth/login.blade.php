f0b827c4191a1b70dcd4302b86e171e5caa51b00
malifrh <alifrhamadhan12@gmail.com>
2021-03-24 19:14:22 +0700

    menambahkan form login sso, membuat style dan link untuk font icon css di login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,50 @@
     <link href="{{ asset('admin/css/style.css')}}" rel="stylesheet">
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <!-- font icon CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
+
+<style>
+.btn-facebook {
+    color: #fff;
+    background-color: #3b5998;
+    border-color: rgba(0,0,0,0.2);
+    width: 248px;
+}
+
+.btn-google {
+    color: #fff;
+    background-color: #E74B37;
+    border-color: rgba(0,0,0,0.2);
+    width: 248px;
+}
+
+.btn-social {
+    position: relative;
+    padding-left: 44px;
+    text-align: left;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+.btn-social:hover {
+    color: #eee;
+}
+
+.btn-social :first-child {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: 40px;
+    padding: 7px;
+    font-size: 1.6em;
+    text-align: center;
+    border-right: 1px solid rgba(0,0,0,0.2);
+}
+</style>
 
 <body>
     <!-- Pre Loader -->
@@ -58,7 +101,27 @@
                                     <div class="form-group text-center">
                                         <button class="btn btn-primary btn-block" type="submit"> Masuk! </button>
                                     </div>
-                                    {{-- <p class="text-dark mb-0">Not a member?<a href="page-register.html"
+
+                                    <div class="form-group text-center">
+                <hr class="w-full"> 
+                <span class="p-2 text-gray-400 mb-1 text-center">OR</span>
+                <hr class="w-full">
+            </div>
+
+            <div class="form-group text-center">
+            <div class="container">
+    <a href="{{ url('auth/facebook') }}" class="btn btn-lg btn-social btn-facebook">
+    <i class="fa fa-facebook fa-fw"></i> Login with Facebook
+    </a>
+</div>
+<br>
+<div class="container">
+    <a href="{{ url('auth/google') }}" class="btn btn-lg btn-social btn-google">
+    <i class="fa fa-google fa-fw"></i> Login with Google
+    </a>
+</div>
+</div>
+          {{-- <p class="text-dark mb-0">Not a member?<a href="page-register.html"
                                             class="text-primary ml-1">Sign Up now</a></p> --}}
                                 </form>
                             </div>
