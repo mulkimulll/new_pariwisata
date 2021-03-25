c9e38ebd09813f51b3b22ce6ec4bb04f6781c923
malifrh <alifrhamadhan12@gmail.com>
2021-03-25 10:16:04 +0700

    menghapus create_roles_table dan menambahkan migration fbb_id & google_id di kolom users

--- database/migrations/2021_03_25_031321_add_google_id_column_in_users_table.php
+++ database/migrations/2021_03_25_031321_add_google_id_column_in_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration
+class AddGoogleIdColumnInUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');  
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('google_id')->nullable();
         });
     }
 
@@ -27,6 +26,9 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('google_id');
+        });
     }
 }
